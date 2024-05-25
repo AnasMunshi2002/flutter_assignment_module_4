36f2cbcbf0f98fb3f02b9a4f83443552aa9538f8
AnasMunshi <anasmunshi2002@gmail.com>
2024-05-25 15:35:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_assignment_module_4
+ Module 4 created by Anas Munshi
---
+++ android/app/src/main/kotlin/com/example/anasmunshi/flutter_assignment_module_4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.anasmunshi.flutter_assignment_module_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
